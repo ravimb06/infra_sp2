80f3964b752d131febbccd245b0ad4ea4dc707f0
ravimb06 <marksheyder06@gmail.com>
2022-07-23 18:01:39 +0300

    final1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,20 +1,12 @@
 import os
 from datetime import timedelta
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-DB_ENGINE='django.db.backends.postgresql' # указываем, что работаем с postgresql
-DB_NAME='postgres' # имя базы данных
-POSTGRES_USER='postgres' # логин для подключения к базе данных
-POSTGRES_PASSWORD='postgres' # пароль для подключения к БД (установите свой)
-DB_HOST='db' # название сервиса (контейнера)
-DB_PORT='5432' # порт для подключения к БД
+load_dotenv()
 
-# надо будет применить потом дотенв
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECRET_KEY = os.getenv('SECRET_KEY', default=None)
-
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
 DEBUG = True
 
@@ -33,7 +25,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'users.apps.UsersConfig',
-   # 'dotenv',
+    'dotenv',
     'rest_framework_simplejwt',
     'djoser',
     'django_filters',
@@ -79,12 +71,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': DB_ENGINE,
-        'NAME': DB_NAME,
-        'USER': POSTGRES_USER,
-        'PASSWORD': POSTGRES_PASSWORD,
-        'HOST': DB_HOST,
-        'PORT': DB_PORT
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432'),
     }
 } 
 
