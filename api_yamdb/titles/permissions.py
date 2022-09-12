5abc2ff09434e8f703fde08cf19eec23dcdbc1fd
Ali Bogatyrev <marksheyder06@gmail.com>
2022-09-12 23:51:37 +0300

    deleted comments

--- api_yamdb/titles/permissions.py
+++ api_yamdb/titles/permissions.py
@@ -12,12 +12,3 @@ class AdminOrReadOnly(permissions.BasePermission):
         return request.method in permissions.SAFE_METHODS or (
             request.user.is_admin and request.user.is_authenticated
         )
-
-# Возможно понадобится сделал на будущее
-# class AuthorOrModerOrReadOnly(permissions.BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         return (request.method in permissions.SAFE_METHODS
-#                 or obj.author == request.user
-#                 or (request.user.is_authenticated
-#                     and request.user.is_moderator_or_admin)
-#                 )
